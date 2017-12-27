7ed4ddee8d8947cb5db0ed6096c14fb2ea954266
Isaac <hawkins.isaac@gmail.com>
2017-12-27 14:19:08 -0500

    add router and materialize css

--- services/passport.js
+++ services/passport.js
@@ -23,18 +23,13 @@ passport.use(
 			callbackURL: '/auth/google/callback',
 			proxy: true
 		},
-		(accessToken, refreshToken, profile, done) => {
-			User.findOne({ googleId: profile.id }).then(existingUser => {
-				if (existingUser) {
-					//user already exists in the database
-					done(null, existingUser);
-				} else {
-					//user doesnt exist, create new user
-					new User({ googleId: profile.id })
-						.save()
-						.then(user => done(null, user));
-				}
-			});
+		async (accessToken, refreshToken, profile, done) => {
+			const existingUser = await User.findOne({ googleId: profile.id });
+			if (existingUser) {
+				return done(null, existingUser);
+			}
+			const user = await new User({ googleId: profile.id }).save();
+			done(null, user);
 		}
 	)
 );
