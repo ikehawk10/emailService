30e3cd9e9c18b1d5701d1527b5031a6c6c9dc82a
Isaac <hawkins.isaac@gmail.com>
2017-12-28 23:27:16 -0500

    add redux-thunk and axios

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Header from './Header';
-const Dashboard = () => <h2>Dashboard</h2>
-const SurveyNew = () => <h2>SurveyNew</h2>
-const Landing = () => <h2>Landing</h2>
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
 	return (
-		<div>
+		<div className="container">
 			<BrowserRouter>
 				<div>
 					<Header />
@@ -18,7 +18,7 @@ const App = () => {
 				</div>
 			</BrowserRouter>
 		</div>
-	)
+	);
 };
 
 export default App;
