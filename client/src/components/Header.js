293a6c22bed1cfbe6f84804fb907dab1860029ef
Isaac <hawkins.isaac@gmail.com>
2017-12-29 11:09:47 -0500

    redirect user to dashboard after login

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+	renderContent() {
+		switch (this.props.auth) {
+			case null:
+				return;
+			case false:
+				return (
+					<li>
+						<a href="/auth/google">Login With Google</a>
+					</li>
+				);
+			default:
+				return (
+					<li>
+						<a>Logout</a>
+					</li>
+				);
+		}
+	}
+
 	render() {
 		return (
 			<nav>
-		    <div className="nav-wrapper">
-		      <a href="#" className="left brand-logo">Emaily</a>
-		      <ul className="right">
-		        <li><a href="sass.html">Login With Google</a></li>
-		      </ul>
-		    </div>
-		  </nav>
-		)
+				<div className="nav-wrapper">
+					<a href="#" className="left brand-logo">
+						Emaily
+					</a>
+					<ul className="right">{this.renderContent()}</ul>
+				</div>
+			</nav>
+		);
 	}
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+	return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
